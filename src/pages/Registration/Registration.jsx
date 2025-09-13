daf740ed0b14722912f4e4ee087b92c1f2f853a7
saurabhrkt <chaurasiasaurabh299@gmail.com>
2025-09-13 12:23:20 +0530

    registration update

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -17,14 +17,9 @@ const Registration = () => (
             </tr>
           </thead>
           <tbody>
-            <tr><td>Student</td><td>₹1000</td><td>₹2360 (₹2000 + 18%)</td></tr>
-            <tr><td>Research Scholar</td><td>₹1000</td><td>₹2360 (₹2000 + 18%)</td></tr>
-            <tr><td>Faculty</td><td>₹5000</td><td>₹5900 (₹5000 + 18%)</td></tr>
-            <tr><td>Industrialist</td><td>₹7000</td><td>₹8260 (₹7000 + 18%)</td></tr>
-            <tr><td>Scientist</td><td>₹7000</td><td>₹8260 (₹7000 + 18%)</td></tr>
+            <tr><td>Student/Research Scholar/Faculty/Industrialist/Scientist</td><td>₹1000</td><td>₹2000 + 18%(GST)</td></tr>
           </tbody>
         </table>
-
         <p className="registration-note">
           *The Registration Fee includes the symposium kit, participation certificate, tea/coffee, and lunch during the event. The last date for registration is <b>21<sup>st</sup> September 2025</b>.
         </p>
